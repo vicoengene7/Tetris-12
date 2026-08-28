58f28f1e1b7619a2937fd3b86be7860446a3e52a
Paz-2007 <mariapazlucia2007@gmail.com>
2026-08-27 21:16:00 -0300

    Implement T piece rotations

--- src/PiezaT.ts
+++ src/PiezaT.ts
@@ -1,6 +1,30 @@
-import {Pieza} from "./Pieza";
+import { Pieza } from "./Pieza";
 
 export class PiezaT extends Pieza {
-
-    
+    constructor() {
+        super(
+            [
+                [
+                    [0, 1, 0],
+                    [1, 1, 1],
+                    [0, 0, 0]
+                ],
+                [
+                    [0, 1, 0],
+                    [0, 1, 1],
+                    [0, 1, 0]
+                ],
+                [
+                    [0, 0, 0],
+                    [1, 1, 1],
+                    [0, 1, 0]
+                ],
+                [
+                    [0, 1, 0],
+                    [1, 1, 0],
+                    [0, 1, 0]
+                ]
+            ]
+        );
+    }
 }
