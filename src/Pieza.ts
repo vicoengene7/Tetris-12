409392896420e1cd7ab0dd2342aaa823fee8e2f1
Paz-2007 <mariapazlucia2007@gmail.com>
2026-08-29 22:58:58 -0300

    Clase pieza con rotacion

--- src/Pieza.ts
+++ src/Pieza.ts
@@ -1,10 +1,27 @@
 export abstract class Pieza {
 
-    protected posiciones: number[][][];
-    protected posicionActual: number;
 
-    constructor(posiciones: number[][][]) {
-        this.posiciones = posiciones;
-        this.posicionActual = 0;
-    }
+protected posiciones: number[][][];
+protected posicionActual: number;
+
+constructor(posiciones: number[][][]) {
+    this.posiciones = posiciones;
+    this.posicionActual = 0;
+}
+
+get forma(): number[][] {
+    return this.posiciones[this.posicionActual]!;
+}
+
+rotarDerecha(): void {
+    this.posicionActual =
+        (this.posicionActual + 1) % this.posiciones.length;
+}
+
+rotarIzquierda(): void {
+    this.posicionActual =
+        (this.posicionActual - 1 + this.posiciones.length)
+        % this.posiciones.length;
+}
+
 }
