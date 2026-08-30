e986c4216bb267cbc810187ffd81b8e3e1253f38
Paz-2007 <mariapazlucia2007@gmail.com>
2026-08-30 20:31:09 -0300

    Test del tablero

---
+++ tests/Tablero.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, test } from "vitest";
+import { Tablero } from "../src/Tablero";
+
+describe("Tablero", () => {
+
+    test("tiene 200 celdas", () => {
+        const tablero = new Tablero();
+
+        expect(tablero.cuadricula.length).toBe(200);
+    });
+
+});
+
+import { PiezaCuadrado } from '../src/PiezaCuadrado';
+test ("añadir una pieza al tablero", () => {
+    const tablero = new Tablero();
+    const pieza = new PiezaCuadrado();
+    const resultado = tablero.añadirPieza(pieza);
+    expect(resultado).toBe(true);
+});
+
+test("una pieza se fija completa en el tablero", () => {
+    const tablero = new Tablero();
+    const pieza = new PiezaCuadrado();
+
+    tablero.añadirPieza(pieza);
+
+    while (tablero.moverAbajo()) {
+       
+    }
+
+    const bloques = tablero.cuadricula.filter(
+        celda => celda === 1
+    );
+
+    expect(bloques.length).toBe(4);
+});
