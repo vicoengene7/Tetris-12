0e9b894d80f744939dd14387fc896588830d1dc7
vicoengene7 <lorevicky100@gmail.com>
2026-08-30 23:54:16 -0300

    Test de limpiarlinea nuevo

--- tests/Tablero.test.ts
+++ tests/Tablero.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi } from "vitest";
+import { describe, expect, test } from "vitest";
 import { Tablero } from "../src/Tablero";
 import { PiezaT } from '../src/PiezaT';
 
@@ -22,28 +22,17 @@ test ("añadir una pieza al tablero", () => {
 
 describe("LimpiarLinea", () => {
 
-  test("si se colocan varias piezas y completan una línea, se suma y se elimina", () => {
+  test("si se completan líneas, se suman y se eliminan", () => {
     const tablero = new Tablero();
-    const columnas = [0, 2, 4, 6, 8];
-    const colMax = 10 - 2; 
 
-    columnas.forEach((columna) => {
-      
-      vi.spyOn(Math, "random").mockReturnValue((columna + 0.5) / (colMax + 1));
-
-      const pieza = new PiezaCuadrado();
-      tablero.añadirPieza(pieza);
-
-      for (let i = 0; i < 19; i++) {
-        tablero.moverAbajo();
-      }
-    });
-
-    vi.restoreAllMocks();
+    for (let i = 0; i < 20; i++) {
+      tablero.cuadricula[i] = 1;
+    }
 
     const lineasEliminadas = tablero.limpiarLinea();
 
     expect(lineasEliminadas).toBe(2);
+});
   });
 
 test("se agrega una pieza después de una rotación", () => {
@@ -55,5 +44,5 @@ test("se agrega una pieza después de una rotación", () => {
     const resultado = tablero.añadirPieza(pieza);
 
     expect(resultado).toBe(true);
-});
+    
 });
